8f198c96c5907e86651e6325fc475277887870ba
zohaib131 <zn058098@gmail.com>
2024-12-16 15:40:53 -0800

    Add files via upload

---
+++ script.js
@@ -0,0 +1,36 @@
+document.getElementById('signup-form').addEventListener('submit', function(event) {
+    event.preventDefault();
+    //after submit the site is open in chrome
+  console.log('submit-btn');
+
+        window.location.href="https://www.clicky.pk/";
+    // Get input values
+    const username = document.getElementById('username').value;
+    const email = document.getElementById('email').value;
+    const password = document.getElementById('password').value;
+    const confirmPassword = document.getElementById('confirm-password').value;
+  
+    let errorMessage = '';
+    
+    // Basic validation
+    if (username === '' || email === '' || password === '' || confirmPassword === '') {
+      errorMessage = 'All fields are required.';
+    } else if (password !== confirmPassword) {
+      errorMessage = 'Passwords do not match.';
+    } else if (password.length < 6) {
+      errorMessage = 'Password should be at least 6 characters long.';
+    }
+    
+    // Display error message if any
+    const errorElement = document.getElementById('error-message');
+    if (errorMessage !== '') {
+      errorElement.textContent = errorMessage;
+    } else {
+      errorElement.textContent = '';
+      alert('Form submitted successfully!');
+
+      
+  
+      
+    }
+  });
